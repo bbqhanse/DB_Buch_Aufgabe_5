bec12146bda198d4d4aef5f1ad992d59037b128a
Christian Gnitka <chriatian@gnitka.eu>
2019-05-24 23:29:35 +0200

    Aufgabe 3 fertig

--- BuchAufgabe5.sql
+++ BuchAufgabe5.sql
@@ -9,3 +9,13 @@ from lehrer L join ausübung A on L.ID_Lehrer = A.ID_Lehrer
 select L.Nachname,L.Kürzel, S.Nachname, K.Bezeichnung
 from klasse K join lehrer L on K.ID_Lehrer = L.ID_Lehrer
     join schüler S on K.ID_Schüler=S.ID_Schüler;
+
+-- Aufgabe 5-3
+select F.Kürzel, L.Kürzel,P.Datum,P.Gewichtung
+from lehrer L join lehrt LE on L.ID_Lehrer = LE.ID_Lehrer
+join fach F on LE.ID_Fach = F.ID_Fach
+join prüfung P on P.ID_Fach=F.ID_Fach;
+
+select F.Kürzel, L.Kürzel,P.Datum,P.Gewichtung
+from prüfung P join fach F on P.ID_Fach = F.ID_Fach
+    join lehrer L on P.ID_Lehrer = L.ID_Lehrer;
