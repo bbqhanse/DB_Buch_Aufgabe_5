1b9cb989b4bffe1eaaf89ee8686d6e72af85f87a
Christian Gnitka <chriatian@gnitka.eu>
2019-05-24 23:30:54 +0200

    Aufgabe 4 Fertig

--- BuchAufgabe5.sql
+++ BuchAufgabe5.sql
@@ -19,3 +19,9 @@ join prüfung P on P.ID_Fach=F.ID_Fach;
 select F.Kürzel, L.Kürzel,P.Datum,P.Gewichtung
 from prüfung P join fach F on P.ID_Fach = F.ID_Fach
     join lehrer L on P.ID_Lehrer = L.ID_Lehrer;
+
+-- Aufgabe 5-4
+select  L.Kürzel, K.Bezeichnung, F.Kürzel
+from unterrichten un join klasse K on un.ID_Klasse =K.ID_Klasse
+join lehrer L on un.ID_Lehrer = L.ID_Lehrer
+join fach F on un.ID_Fach = F.ID_Fach;
