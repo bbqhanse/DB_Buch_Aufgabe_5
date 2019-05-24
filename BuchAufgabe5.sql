30d6f97d034f8e486d272652e2b273c16bfabb7a
Christian Gnitka <chriatian@gnitka.eu>
2019-05-24 23:42:57 +0200

    Nur Aufgabe 1 Sichtbar

--- BuchAufgabe5.sql
+++ BuchAufgabe5.sql
@@ -5,7 +5,3 @@ select L.Nachname, L.Kürzel,S.Bezeichnung
 from lehrer L join ausübung A on L.ID_Lehrer = A.ID_Lehrer
     join amt S on  A.ID_Amt = S.ID_Amt;
 
--- Aufgabe 5-2
-select L.Nachname,L.Kürzel, S.Nachname, K.Bezeichnung
-from klasse K join lehrer L on K.ID_Lehrer = L.ID_Lehrer
-    join schüler S on K.ID_Schüler=S.ID_Schüler;
